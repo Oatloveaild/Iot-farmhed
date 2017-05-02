9c243957cf5eb03c8a6862dcfb12373b0defab36
Oatloveaild <industry9110@gmail.com>
2017-05-03 04:30:53 +0700

    test up

--- app/src/main/java/com/example/user/iot_farmhed/MainActivity.java
+++ app/src/main/java/com/example/user/iot_farmhed/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.user.iot_farmhed;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+/**
+ * Created by user on 3/5/2560.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    private Button adva;
+    private TextView rhum, rtemp;
+    private static  final String TAG = "MainActivity";
+
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adva = (Button) findViewById(R.id.proc);
+
+        Intent in = getIntent();
+        rhum = (TextView) findViewById(R.id.rhum);
+        rtemp = (TextView) findViewById(R.id.rtemp);
+        adva.setOnClickListener(new View.OnClickListener(){
+            public  void  onClick(View view) {
+                Intent in = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(in);
+            }
+        });
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        Query lastQuery = databaseReference.child("logDHT").orderByKey().limitToLast(1);
+        lastQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String humidity = dataSnapshot.child("humidity").getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/user/iot_farmhed/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.user.iot_farmhed;
+
+/**
+ * Created by user on 3/5/2560.
+ */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity2 {
+
+}
+
